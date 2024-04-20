e646dec8c1bcf64ea94f322d62089eec76bad872
Yassine <mohamedyassinerachid@gmail.com>
2024-04-20 00:20:00 -0400

    remove useless comment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static String arrayClockWise(int[][] matrix) {
@@ -30,8 +28,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         int[][] matrix = new int[][]{
                 {1, 2, 3, 4, 10},
                 {10, 11, 12, 5, 20},
